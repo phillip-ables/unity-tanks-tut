660e9370647c8ab996fcde88209830bfe2c4b777
lip ables <phillipables1@gmail.com>
2019-01-26 17:48:29 -0500

    comments removed from game manager script

--- multiTank/Assets/Scripts/Managers/GameManager.cs
+++ multiTank/Assets/Scripts/Managers/GameManager.cs
@@ -17,9 +17,9 @@ public class GameManager : MonoBehaviour
     private int m_RoundNumber;              
     private WaitForSeconds m_StartWait;     
     private WaitForSeconds m_EndWait;       
-/*    private TankManager m_RoundWinner;
+    private TankManager m_RoundWinner;
     private TankManager m_GameWinner;       
-*/
+
 
     private void Start()
     {
@@ -64,7 +64,7 @@ public class GameManager : MonoBehaviour
         yield return StartCoroutine(RoundPlaying());
         yield return StartCoroutine(RoundEnding());
 
-/*        if (m_GameWinner != null)
+        if (m_GameWinner != null)
         {
             SceneManager.LoadScene(0);
         }
@@ -72,7 +72,7 @@ public class GameManager : MonoBehaviour
         {
             StartCoroutine(GameLoop());
         }
-*/    }
+    }
 
 
     private IEnumerator RoundStarting()
@@ -106,7 +106,6 @@ public class GameManager : MonoBehaviour
         return numTanksLeft <= 1;
     }
 
-/*
     private TankManager GetRoundWinner()
     {
         for (int i = 0; i < m_Tanks.Length; i++)
@@ -150,7 +149,6 @@ public class GameManager : MonoBehaviour
 
         return message;
     }
-*/
 
     private void ResetAllTanks()
     {
